777446fc3c79aefc5a0ea75800528e0414344113
nzlyrnkcr <nzlyrnkcr@gmail.com>
2026-03-26 23:54:22 +0300

    Add files via upload

---
+++ Kulllanıcıdan iki değer alıp onların toplamını yazma.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+int sayi1, sayi2;
+cout<<"Sayilari giriniz: "<<endl;
+cin>>sayi1>>sayi2;
+int toplam=0;
+toplam=sayi1+sayi2;
+cout<<"Toplam="<<toplam;
+return 0;
+}

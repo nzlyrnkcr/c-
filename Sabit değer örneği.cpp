66705e63c0bc1405183b3a16a21732db34baa352
nzlyrnkcr <nzlyrnkcr@gmail.com>
2026-03-27 00:00:27 +0300

    Add files via upload

---
+++ Sabit değer örneği.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace  std;
+int main(){
+float yaricap, alan=1;
+float const pi=3.14;
+cout<<"Yaricapi Giriniz: "<<endl;
+cin>>yaricap;
+alan=pi*yaricap*yaricap;
+cout<<"Cemberin Alani="<<alan;
+return 0;
+}
